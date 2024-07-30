3f0a15e7e86fd53d5abe1efb354c31b6cd262a47
slashedCodes <alexfeed1990real@gmail.com>
2024-07-30 15:03:09 +0300

    Add Geometry Dash WM Profile

---
+++ archinstall/default_profiles/desktops/gd.py
@@ -0,0 +1,23 @@
+from typing import List, Optional, Any, TYPE_CHECKING
+
+from archinstall.default_profiles.profile import ProfileType, GreeterType
+from archinstall.default_profiles.gd import GDProfile
+
+if TYPE_CHECKING:
+	_: Any
+
+
+class GDProfile(GDProfile):
+	def __init__(self):
+		super().__init__('GD', ProfileType.DesktopEnv, description='')
+
+	@property
+	def packages(self) -> List[str]:
+		return [
+			"geometry-dash-wm",
+			"geometry-dash-plymouth-theme"
+		]
+
+	@property
+	def default_greeter_type(self) -> Optional[GreeterType]:
+		return GreeterType.Gdm
